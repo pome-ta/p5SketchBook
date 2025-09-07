5c9cc223d39d8b26d7735dcd39e32068805abab8
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-09-07 13:19:05 +0900

    mac

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,31 @@
 import dirTree from 'dirTree' with {type: 'json'};
 
-console.log(dirTree);
+console.log(dirTree.length);
+
+
+for (let [key, value] of Object.entries(dirTree.children)) {
+  // console.log('#################');
+  // console.log(key, value);
+
+}
+
+
+
+const walk = (treeNode, parent) => {
+  const li = document.createElement('li');
+  li.textContent = treeNode.name;
+  if (treeNode.type === 'dir') {
+    const ulSub = document.createElement('ul');
+    walk(treeNode.children, ulSub);
+    li.appendChild(ulSub);
+  }
+  parent.appendChild(li);
+}
+
+const ul = document.createElement('ul');
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOMContentLoaded');
+  document.body.appendChild(ul);
+  walk(dirTree, ul);
+});

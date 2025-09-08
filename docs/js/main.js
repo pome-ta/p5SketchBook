86e740366a46a5e1b0f57ce48bd88d74e575ba70
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-09-08 11:20:47 +0900

    up

--- docs/js/main.js
+++ docs/js/main.js
@@ -10,7 +10,15 @@ const walk = (treeNodes, parent) => {
       return;
     }
 
-    const li = document.createElement('li');
+    //const li = document.createElement('li');
+    const li = DomFactory.create('li', {
+      setStyles: {
+        width: 'auto',
+        //overflow: 'hidden',
+        //'text-overflow': 'ellipsis',
+        'white-space': 'nowrap',
+      },
+    });
     li.textContent = `${treeNode.type === 'file' ? '📄' : '📁'} ${treeNode.name}`;
     if (treeNode.type === 'dir') {
       const ulSub = document.createElement('ul');
@@ -26,15 +34,19 @@ const ul = DomFactory.create('ul', {
   setStyles: {
     'font-family':
       'Consolas, Menlo, Monaco, source-code-pro, Courier New, monospace',
-    //'font-size': '0.8rem',
-    padding: '0.5rem 1rem',
+    'font-size': '0.8rem',    
+    //padding: '0.5rem 1rem',
+    //overflow: 'hidden',
   },
 });
 
 walk(dirTree, ul);
+const details = DomFactory.create('details')
+details.appendChild(ul)
 
 document.addEventListener('DOMContentLoaded', () => {
   //console.log('DOMContentLoaded');
-  document.body.appendChild(ul);
+  //document.body.appendChild(ul);
+  document.body.appendChild(details);
 
 });

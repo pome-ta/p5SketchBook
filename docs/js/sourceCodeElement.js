e8ae5dda00101a2b0e6d7a55bbb2a1fa5f8e0f30
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-09-19 16:07:39 +0900

    up

--- docs/js/sourceCodeElement.js
+++ docs/js/sourceCodeElement.js
@@ -16,23 +16,19 @@ export default class SourceCodeElement {
   constructor(querySel) {
     this.#querySel = querySel;
     this.#hljsValue = '';
-    this.#preTag = DomFactory.create('pre');
+    this.#preTag = DomFactory.create('pre', {
+      setStyles: {
+        'font-family':
+          'Consolas, Menlo, Monaco, source-code-pro, Courier New, monospace',
+        'font-size': '0.6rem',
+        'white-space': 'pre-wrap',
+      },
+    });
     this.#showButton = this.#createShowButton();
     this.#dialog = this.#createDialog();
   }
   
   #createDialog() {
-    /*
-    const wrap = DomFactory.create('div', {
-      setStyles: {
-        'font-family':
-          'Consolas, Menlo, Monaco, source-code-pro, Courier New, monospace',
-        'font-size': '0.8rem',
-        //width: '100%',
-      },
-      addClassList: ['dialog-container', ],
-    });
-    */
     
     const wrapper = DomFactory.create('div', {
       addClassList: ['dialog-container', ],
@@ -63,6 +59,19 @@ export default class SourceCodeElement {
       ],
     });
     
+    const copyButton = DomFactory.create('button', {
+      setStyles: {
+        'border-radius': '0.5rem',
+        margin: '0.5rem 0',
+      },
+      appendChildren: [
+        DomFactory.create('p', {
+          textContent: 'copy',
+        }),
+      ],
+    });
+    
+    
     const dialog = DomFactory.create('dialog', {
       setStyles: {
         width: '88%',
@@ -71,7 +80,10 @@ export default class SourceCodeElement {
         'border-radius': '0.5rem',
         'box-shadow': '0 4px 16px rgba(0 0 0 / 16%)',
       },
-      appendChildren: [closeButton, wrapper],
+      appendChildren: [
+        closeButton,
+        wrapper,
+      ],
       addEventListeners: [
         {
           type: 'click',
@@ -91,9 +103,9 @@ export default class SourceCodeElement {
           listener: {
             handleEvent: (event) => {
               const codeStr = document.querySelector(this.#querySel).innerText;
-              const highlightedCode = hljs.highlightAuto(codeStr).value
+              this.#hljsValue = hljs.highlightAuto(codeStr).value
 
-              this.#preTag.innerHTML = highlightedCode;
+              this.#preTag.innerHTML = this.#hljsValue;
               dialog.showModal();
             },
           },

47a259002cfe442c8131a34ccd997031db10d827
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-09-20 22:38:44 +0900

    up

--- docs/js/dirTreeElement.js
+++ docs/js/dirTreeElement.js
@@ -1,21 +1,12 @@
-export default class SourceCodeElement {
-  #getItemId;
-  #codeStr;
-  #preTag;
+import DomFactory from './utils/domFactory.js';
+
+export default class DirTreeElement {
+  
   #dialog;
   #showButton;
 
-  constructor(getItemId) {
-    this.#getItemId = getItemId;
-    this.#codeStr = '';
-    this.#preTag = DomFactory.create('pre', {
-      setStyles: {
-        'font-family':
-          'Consolas, Menlo, Monaco, source-code-pro, Courier New, monospace',
-        'font-size': '0.6rem',
-        'white-space': 'pre-wrap',
-      },
-    });
+  constructor() {
+    
     this.#showButton = this.#createShowButton();
     this.#dialog = this.#createDialog();
   }
